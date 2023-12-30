66df15b0b314f8c0d6f63d7ababc6d32b9c4aa7e
Kasra <kasra76@gmail.com>
2023-12-30 18:12:20 +0330

    Recreated the project for typo reasons

--- shipmentTracker/shipmentTracker/asgi.py
+++ shipmentTracker/shipmentTracker/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for shipmentTrancker project.
+ASGI config for shipmentTracker project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shipmentTrancker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shipmentTracker.settings')
 
 application = get_asgi_application()
--- shipmentTracker/shipmentTracker/urls.py
+++ shipmentTracker/shipmentTracker/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for shipmentTrancker project.
+URL configuration for shipmentTracker project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
