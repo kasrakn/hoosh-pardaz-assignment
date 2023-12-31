bee71591122be883fcf87641404ccd9f3504d4eb
Kasra <kasra76@gmail.com>
2023-12-31 15:38:03 +0330

    Fixed the bug in reading data from data.csv file

--- shipmentTracker/tracking/management/commands/populate_data.py
+++ shipmentTracker/tracking/management/commands/populate_data.py
@@ -1,4 +1,5 @@
 from typing import Any
+import csv
 from django.core.management.base import BaseCommand
 from tracking.models import Shipment
 
@@ -6,21 +7,26 @@ class Command(BaseCommand):
     help = 'Populate the database with the initial data'
 
     def handle(self, *args: Any, **options: Any) -> None:
-        with open('data.csv', 'r') as file:
-            for line in file[1:]:
-                cleaned_data = line.strip()
-                data = cleaned_data.split(',')
+        with open('data.csv', 'r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
 
-                # Create model objects with the data
-                Shipment.objects.create(
-                    tracking_number=data[0],
-                    carrier=data[1],
-                    sender_address=data[2],
-                    receiver_address=data[3],
-                    article_name=data[4],
-                    article_quantity=data[5],
-                    article_price=data[6],
-                    sku=data[7],
-                    status=data[8]
-                )
+            # loop over the rows of the csv file
+            for row in csv_reader:
+                if line_count != 0:
+
+                    # Create model objects with the data
+                    Shipment.objects.create(
+                        tracking_number=row[0],
+                        carrier=row[1],
+                        sender_address=row[2],
+                        receiver_address=row[3],
+                        article_name=row[4],
+                        article_quantity=row[5],
+                        article_price=row[6],
+                        sku=row[7],
+                        status=row[8]
+                    )
+                    # Shipment.save()
+                line_count += 1
             self.stdout.write(self.style.SUCCESS('Initial data successfully populated'))
